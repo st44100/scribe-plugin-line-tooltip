c083793481316e84a4db9fa6524c3a269f435372
st44100 <st44100@gmail.com>
2016-03-07 15:05:52 +0900

    Revert: tooltipTempalte

--- src/scribe-plugin-line-tooltip.js
+++ src/scribe-plugin-line-tooltip.js
@@ -61,7 +61,7 @@ export default class ScribePluginLineTooltip {
     tooltipEl.classList.add(`tooltip`)
     tooltipEl.classList.add(`js-tooltip`)
     tooltipEl.innerHTML = `
-      <p class="tooltip__item"> <i class="icon icon--plus"></i> </p>
+      <p class="tooltip__item"> + </p>
     `
     tooltipOuter.appendChild(tooltipEl)
 
